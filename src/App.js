c54a19e9ca998cd28b8479069b1f34264b8885c1
nairdesigns <nairbdg@gmail.com>
2022-04-15 10:53:46 +0800

    compoennt flat list working!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
+import FlatList from "./components/flat_list";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. yoyo
         </p>
         <a
           className="App-link"
@@ -17,6 +18,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          <FlatList />
         </a>
       </header>
     </div>
