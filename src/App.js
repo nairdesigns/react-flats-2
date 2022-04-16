647c31040cf0fcb46a8cf762cabaa007885c1d11
nairdesigns <nairbdg@gmail.com>
2022-04-16 10:56:09 +0800

    switch to class component instead of functional compenent

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import React from "react";
 import FlatList from "./components/flat_list";
 
 function App() {
+  selectFlat = (index) => {
+    this.setState({ selectedFlat: flats[index] });
+  };
   return (
     <div className="App">
       <FlatList />
