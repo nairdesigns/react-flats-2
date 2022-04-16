fd807d02dbc50d1f4f9ba250ac395a9e203562ee
nairdesigns <nairbdg@gmail.com>
2022-04-17 00:01:39 +0800

    experimenting with mapbox

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import FlatList from "./components/flat_list";
 // import GoogleMapReact from "google-map-react";
+import { useRef, useEffect, useState } from "react";
 
 import React, { Component } from "react";
 import flats from "./data/flats";
 import Marker from "./components/marker";
+import mapboxgl from "!mapbox-gl";
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibmFpcmRlc2lnbnMiLCJhIjoiY2t6N3oxZGtyMWY5ejJ2cGhtc283bWtoYiJ9.2Xm0lvF_0guXITdMgqokVg";
 
 export default class App extends Component {
   constructor(props) {
@@ -15,15 +19,15 @@ export default class App extends Component {
     };
   }
 
-  center() {
-    return {
-      lat: this.state.selectedFlat.lat,
-      lng: this.state.selectedFlat.lng,
-    };
+  // center() {
+  //   return {
+  //     lat: this.state.selectedFlat.lat,
+  //     lng: this.state.selectedFlat.lng,
+  //   };
   }
-  selectFlat = (index) => {
-    this.setState({ selectedFlat: flats[index] });
-  };
+  // selectFlat = (index) => {
+  //   this.setState({ selectedFlat: flats[index] });
+  // };
   render() {
     return (
       <div>
