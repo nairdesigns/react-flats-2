21eedae674cfb95bcd7a3e4e1b14599ed457e286
nairdesigns <nairbdg@gmail.com>
2022-04-16 22:47:06 +0800

    cards are working fine. still not as reactive as i iwnat

--- src/App.js
+++ src/App.js
@@ -27,7 +27,6 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        App
         <FlatList
           flats={this.state.flats}
           selectedFlat={this.state.selectedFlat}
