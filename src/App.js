b630cfdc4a5d51a0cf814345c62efe888ff3dde8
nairdesigns <nairbdg@gmail.com>
2022-04-16 11:26:22 +0800

    moved data folder to src

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import logo from "./logo.svg";
 import "./App.css";
 import FlatList from "./components/flat_list";
 import React, { Component } from "react";
-
+import flats from "./data/flats";
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedFlat: flats[0],
+      flats,
+    };
+  }
+
   render() {
     return (
       <div>
@@ -13,14 +21,3 @@ export default class App extends Component {
     );
   }
 }
-
-// function App() {
-
-//   return (
-//     <div className="App">
-//       <FlatList />
-//     </div>
-//   );
-// }
-
-// export default App;
