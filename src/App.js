55a89124cc47d59d20d540d0ef74da3d33e0ee75
nairdesigns <nairbdg@gmail.com>
2022-04-17 19:36:16 +0800

    added style sheet for map box at the head of html

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import React, { Component } from "react";
 import flats from "./data/flats";
 import Marker from "./components/marker";
 import mapboxgl from "mapbox-gl";
-import Map from "react-map-gl";
+// import Map from "react-map-gl";
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoibmFpcmRlc2lnbnMiLCJhIjoiY2t6N3oxZGtyMWY5ejJ2cGhtc283bWtoYiJ9.2Xm0lvF_0guXITdMgqokVg";
